16841fa7602d910843ff21cfa5ec2465b94be6fb
shulaoda <laipichan@qq.com>
2024-12-25 16:18:46 +0800

    refactor: inline determine_es_module

--- crates/minipack/src/utils/chunk/namespace_marker.rs
+++ crates/minipack/src/utils/chunk/namespace_marker.rs
@@ -1,15 +1,5 @@
 use minipack_common::EsModuleFlag;
 
-/// Combine the `es_module_flag` and whether it has a default export to determine if there need
-/// to have a namespace marker in the output.
-pub fn determine_es_module(es_module_flag: EsModuleFlag, has_default_export: bool) -> bool {
-  match es_module_flag {
-    EsModuleFlag::Always => true,
-    EsModuleFlag::IfDefaultProp if has_default_export => true,
-    _ => false,
-  }
-}
-
 /// Render namespace markers for the module.
 /// It contains the `__esModule` and `Symbol.toStringTag` properties.
 /// Since rolldown doesn't support `generatedCode.symbol` yet,
@@ -20,7 +10,12 @@ pub fn render_namespace_markers(
   // TODO namespace_to_string_tag
   namespace_to_string_tag: bool,
 ) -> Option<&'static str> {
-  let es_module = determine_es_module(es_module_flag, has_default_export);
+  let es_module = match es_module_flag {
+    EsModuleFlag::Always => true,
+    EsModuleFlag::IfDefaultProp if has_default_export => true,
+    _ => false,
+  };
+
   if es_module && namespace_to_string_tag {
     Some("Object.defineProperties(exports, { __esModule: { value: true }, [Symbol.toStringTag]: { value: 'Module' } });")
   } else if es_module {
